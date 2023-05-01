7ee0ead510708a979e6ade01e86d8216f94115a9
unknown <hebaalhwai96@gmail.com>
2023-05-01 12:50:58 +0300

    changing mysql query

--- connect_to_db.php
+++ connect_to_db.php
@@ -8,11 +8,13 @@ class Connect
         try {
             $dsn = 'mysql:dbname=php_project;host=127.0.0.1;port=3306;';
             $db = new PDO($dsn, self::DB_USER, self::DB_PASSWORD);
-            $sh = $db->prepare("DESCRIBE `products_orders`");
-            if ($sh->execute()) {
+            $sh = $db->prepare("SHOW TABLES LIKE 'products_orders'");
+           $sh->execute();
+             if ($sh->fetch(PDO::FETCH_ASSOC)) {
                 var_dump("Tables exists");
                 die();
-            } else {
+             }
+             else {
                 Connect::createTables($db);
                 var_dump("Tables created");
                 die();
