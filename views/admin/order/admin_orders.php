ec1a0f269442883dee88f4bbe54c1935b280b429
Nora Mohammed <NoorMohammed3399@gmail.com>
2023-05-09 01:21:02 +0300

    update order files

--- views/admin/order/admin_orders.php
+++ views/admin/order/admin_orders.php
@@ -8,29 +8,30 @@ echo '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstra
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>';
 
 echo "
-<h1 style='margin-left:380px;'class='mt-5'> Orders</h1>
-<table class='table table-bordered table-striped text-center w-50 mt-5'style='margin:auto;'>
-    <thead>
-        <tr>
+<h1 style='margin-left:200px;'class='mt-5'>Orders</h1>
+<table class='table table-striped table-bordered table-hover text-center w-75 mt-5'style='margin:auto;>
+        <tr class='table-secondary'>
             <th>Order Date</th>
             <th>Name</th>
             <th>Room</th>
             <th>Ext</th>
             <th>Action</th>
         </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <th >1</th>
+        <tr class='table-secondary'>
+            <td>2018/02/02 03:30 Am </td>
             <td>Mark</td>
             <td>Otto</td>
             <td>@mdo</td>
+            <td></td>
         </tr>
-        <tr>
-            <th>2</th>
+        <tr class='table-secondary'>
+            <td>2018/02/02 03:30 Am</td>
             <td>Jacob</td>
             <td>Thornton</td>
             <td>@fat</td>
+            <td></td>
         </tr>
-    </tbody>";
+        <tr class='table-secondary'>
+            <td colspan='5'><br><br><br><h4 style='margin-left:80%;'>Total : &nbsp &nbsp &nbsp EGP</h4> </td>
+        </tr>";
 echo "</table>";
--- views/user/user_order.php
+++ views/user/user_order.php
@@ -8,35 +8,41 @@ echo '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstra
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>';
 
 echo "
-<h1 style='margin-left:380px;'class='mt-5'>My Orders</h1>
+<h1 style='margin-left:200px;'class='mt-5'>My Orders</h1>
     <div class='d-flex mb-3'>
-        <input type='text' placeholder='Date From' name='title' class='form-control ' id='title'style='margin-left:380px; margin-right:20px; width:300px;'>
-        <img src='/images/49.jpg'>
-        <input type='text' placeholder='Date To' name='title' class='form-control ' id='title' style='width:300px;' >
-        <img src='/images/49.jpg'>
+        <input type='text' placeholder='Date From' name='title' class='form-control ' id='title'style='margin-left:200px; margin-right:20px; width:400px; border:4px solid gray;'>
+        <img src='date.jpg' style='width:50px;heught:50px;'>
+        <input type='text' placeholder='Date To' name='title' class='form-control ' id='title' style='width:400px; border:4px solid gray;' >
+        <img src='date.jpg' style='width:50px;heught:50px;'>
     </div>
-<table class='table table-bordered table-striped text-center w-50 mt-5'style='margin:auto;'>
-    <thead>
-        <tr>
+    <table class='table table-striped table-bordered table-hover text-center w-75 mt-5'style='margin:auto;>
+        <tr class='table-secondary'>
             <th>Order Date</th>
             <th>Status</th>
             <th>Amount</th>
             <th>Action</th>
         </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <th >1</th>
+        <tr class='table-secondary'>
+            <td>1111111111 <a class='btn btn-warning'>+</a></td>
             <td>Mark</td>
             <td>Otto</td>
             <td>@mdo</td>
         </tr>
-        <tr>
-            <th>2</th>
+        <tr class='table-secondary'>
+            <td>2222222222 <a class='btn btn-warning'>+</a></td>
             <td>Jacob</td>
             <td>Thornton</td>
             <td>@fat</td>
         </tr>
-    </tbody>";
+        <tr class='table-secondary'>
+            <td>3333333333 <a class='btn btn-warning'>+</a></td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+        </tr>";
 echo "</table>";
-echo "<h3 style='margin-left:850px;'class='mt-5'>Total</h3>";
+echo "
+    <div style='margin-left:300px; border:3px solid gray;' class='w-50 mt-5 d-flex justify-content-around align-items-baseline'>
+        <h3 class='m-2'>Total : </h3>
+        <h3 class='m-2'>EGP</h3>
+    </div>";
