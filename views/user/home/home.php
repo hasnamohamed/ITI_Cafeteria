0e7089ed316851d00245dac40a726e92bdebe9fc
MohamedAl-Attar <mohamedelattar28@yahoo.com>
2023-05-09 03:08:28 +0300

    Users Home v1

---
+++ views/user/home/home.php
@@ -0,0 +1,339 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+          integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+          crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Rubik+Gemstones&display=swap" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+            integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+            crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+            integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+            crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+            integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+            crossorigin="anonymous"></script>
+    <style>
+
+
+        form .i {
+
+            width: 5%;
+            height: auto;
+
+        }
+
+        .btn {
+
+            border-radius: 40px;
+            width: 35%;
+
+        }
+
+        .f {
+            border-radius: 40px;
+            height: 100%;
+
+        }
+
+        .di {
+
+            display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+            justify-content: center;
+
+        }
+
+        .navbar a:hover{
+
+            background-color:gainsboro;
+            transform: scale(1,1);
+
+        }
+        .col:hover{
+            box-shadow: 10px 10px 8px 10px #888888;
+
+        }
+        h5{
+
+
+            text-shadow: 5px 5px 5px grey ;
+            font-size: 60px;
+            font-family: 'Rubik Gemstones', cursive;
+        }
+        p{
+            font-family: Arial, Helvetica, sans-serif;
+            text-shadow: 5px 5px 5px grey ;
+        }
+        span{
+
+            font-family: Arial, Helvetica, sans-serif;
+            text-shadow: 5px 5px 5px grey ;
+
+        }
+
+
+    </style>
+</head>
+
+<body>
+<section class="services py-5 bg-light px-5 d-flex" style="height: 100rem">
+    <div class="container d-flex " style="width: 25%; height: 100% ; background-color: #0db8de" >
+<!--    <div class="card" style="width: 10rem; height: 8rem; margin-left: 5px" >-->
+<!--        <img class="card-img-top" style="height: 45%; width:70%; align-self: center" src="../../../public/images/Coffee_cup_icon.svg.png">-->
+<!--        <div class="card-body d-flex justify-content-between">-->
+<!--            <p class="card-text">Coffee</p>-->
+<!--            <p class="card-text">5$</p>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--    ------------------------------------------------------------------->
+<!--    <div class="card" style="width: 10rem; height: 8rem;margin-left: 5px">-->
+<!--        <img class="card-img-top" style="height: 45%; width:70%; align-self: center" src="../../../public/images/Coffee_cup_icon.svg.png">-->
+<!--        <div class="card-body d-flex justify-content-between">-->
+<!--            <p class="card-text">Coffee</p>-->
+<!--            <p class="card-text">5$</p>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--    <div class="card" style="width: 10rem; height: 8rem; margin-left: 5px">-->
+<!--        <img class="card-img-top" style="height: 45%; width:70%; align-self: center" src="../../../public/images/Coffee_cup_icon.svg.png">-->
+<!--        <div class="card-body d-flex justify-content-between">-->
+<!--            <p class="card-text">Coffee</p>-->
+<!--            <p class="card-text">5$</p>-->
+<!--        </div>-->
+<!--    </div>-->
+<!---->
+<!--    <div class="card" style="width: 10rem; height: 8rem; margin-left: 5px">-->
+<!--        <img class="card-img-top" style="height: 45%; width:70%; align-self: center" src="../../../public/images/Coffee_cup_icon.svg.png">-->
+<!--        <div class="card-body d-flex justify-content-between">-->
+<!--            <p class="card-text">Coffee</p>-->
+<!--            <p class="card-text">5$</p>-->
+<!--        </div>-->
+<!--    </div>-->
+<!---->
+<!--    <div class="card" style="width: 10rem; height: 8rem; margin-left: 5px">-->
+<!--        <img class="card-img-top" style="height: 45%; width:70%; align-self: center" src="../../../public/images/Coffee_cup_icon.svg.png">-->
+<!--        <div class="card-body d-flex justify-content-between">-->
+<!--            <p class="card-text">Coffee</p>-->
+<!--            <p class="card-text">5$</p>-->
+<!--        </div>-->
+<!--    </div>-->
+<!---->
+<!--    <div class="card" style="width: 10rem; height: 8rem; margin-left: 5px">-->
+<!--        <img class="card-img-top" style="height: 45%; width:70%; align-self: center" src="../../../public/images/Coffee_cup_icon.svg.png">-->
+<!--        <div class="card-body d-flex justify-content-between">-->
+<!--            <p class="card-text">Coffee</p>-->
+<!--            <p class="card-text">5$</p>-->
+<!--        </div>-->
+<!--    </div>-->
+<!---->
+<!--    <div class="card" style="width: 10rem; height: 8rem; margin-left: 5px">-->
+<!--        <img class="card-img-top" style="height: 45%; width:70%; align-self: center" src="../../../public/images/Coffee_cup_icon.svg.png">-->
+<!--        <div class="card-body d-flex justify-content-between">-->
+<!--            <p class="card-text">Coffee</p>-->
+<!--            <p class="card-text">5$</p>-->
+<!--        </div>-->
+<!--    </div>-->
+    </div>
+
+    <div style="width: 65%; height: 100% ;" class="d-flex flex-wrap">
+            <div class="card" style="width: 10rem; height: 8rem;margin-left: 5px">
+                <img class="card-img-top" style="height: 45%; width:70%; align-self: center" src="../../../public/images/Coffee_cup_icon.svg.png">
+                <div class="card-body d-flex justify-content-between">
+                    <p class="card-text">Coffee</p>
+                    <p class="card-text">5$</p>
+                </div>
+            </div>
+            <div class="card" style="width: 10rem; height: 8rem; margin-left: 5px">
+                <img class="card-img-top" style="height: 45%; width:70%; align-self: center" src="../../../public/images/Coffee_cup_icon.svg.png">
+                <div class="card-body d-flex justify-content-between">
+                    <p class="card-text">Coffee</p>
+                    <p class="card-text">5$</p>
+                </div>
+            </div>
+
+            <div class="card" style="width: 10rem; height: 8rem; margin-left: 5px">
+                <img class="card-img-top" style="height: 45%; width:70%; align-self: center" src="../../../public/images/Coffee_cup_icon.svg.png">
+                <div class="card-body d-flex justify-content-between">
+                    <p class="card-text">Coffee</p>
+                    <p class="card-text">5$</p>
+                </div>
+            </div>
+
+            <div class="card" style="width: 10rem; height: 8rem; margin-left: 5px">
+                <img class="card-img-top" style="height: 45%; width:70%; align-self: center" src="../../../public/images/Coffee_cup_icon.svg.png">
+                <div class="card-body d-flex justify-content-between">
+                    <p class="card-text">Coffee</p>
+                    <p class="card-text">5$</p>
+                </div>
+            </div>
+
+            <div class="card" style="width: 10rem; height: 8rem; margin-left: 5px">
+                <img class="card-img-top" style="height: 45%; width:70%; align-self: center" src="../../../public/images/Coffee_cup_icon.svg.png">
+                <div class="card-body d-flex justify-content-between">
+                    <p class="card-text">Coffee</p>
+                    <p class="card-text">5$</p>
+                </div>
+            </div>
+
+            <div class="card" style="width: 10rem; height: 8rem; margin-left: 5px; top: 0">
+                <img class="card-img-top" style="height: 45%; width:70%; align-self: center" src="../../../public/images/Coffee_cup_icon.svg.png">
+                <div class="card-body d-flex justify-content-between">
+                    <p class="card-text">Coffee</p>
+                    <p class="card-text">5$</p>
+                </div>
+            </div>
+
+        <div class="card" style="width: 10rem; height: 8rem; margin-left: 5px">
+            <img class="card-img-top" style="height: 45%; width:70%; align-self: center" src="../../../public/images/Coffee_cup_icon.svg.png">
+            <div class="card-body d-flex justify-content-between">
+                <p class="card-text">Coffee</p>
+                <p class="card-text">5$</p>
+            </div>
+        </div>
+
+        <div class="card" style="width: 10rem; height: 8rem; margin-left: 5px">
+            <img class="card-img-top" style="height: 45%; width:70%; align-self: center" src="../../../public/images/Coffee_cup_icon.svg.png">
+            <div class="card-body d-flex justify-content-between">
+                <p class="card-text">Coffee</p>
+                <p class="card-text">5$</p>
+            </div>
+        </div>
+
+        <div class="card" style="width: 10rem; height: 8rem; margin-left: 5px">
+            <img class="card-img-top" style="height: 45%; width:70%; align-self: center" src="../../../public/images/Coffee_cup_icon.svg.png">
+            <div class="card-body d-flex justify-content-between">
+                <p class="card-text">Coffee</p>
+                <p class="card-text">5$</p>
+            </div>
+        </div>
+
+        <div class="card" style="width: 10rem; height: 8rem; margin-left: 5px">
+            <img class="card-img-top" style="height: 45%; width:70%; align-self: center" src="../../../public/images/Coffee_cup_icon.svg.png">
+            <div class="card-body d-flex justify-content-between">
+                <p class="card-text">Coffee</p>
+                <p class="card-text">5$</p>
+            </div>
+        </div>
+
+        <div class="card" style="width: 10rem; height: 8rem; margin-left: 5px">
+            <img class="card-img-top" style="height: 45%; width:70%; align-self: center" src="../../../public/images/Coffee_cup_icon.svg.png">
+            <div class="card-body d-flex justify-content-between">
+                <p class="card-text">Coffee</p>
+                <p class="card-text">5$</p>
+            </div>
+        </div>
+
+        <div class="card" style="width: 10rem; height: 8rem; margin-left: 5px">
+            <img class="card-img-top" style="height: 45%; width:70%; align-self: center" src="../../../public/images/Coffee_cup_icon.svg.png">
+            <div class="card-body d-flex justify-content-between">
+                <p class="card-text">Coffee</p>
+                <p class="card-text">5$</p>
+            </div>
+        </div>
+
+        <div class="card" style="width: 10rem; height: 8rem; margin-left: 5px">
+            <img class="card-img-top" style="height: 45%; width:70%; align-self: center" src="../../../public/images/Coffee_cup_icon.svg.png">
+            <div class="card-body d-flex justify-content-between">
+                <p class="card-text">Coffee</p>
+                <p class="card-text">5$</p>
+            </div>
+        </div>
+
+        <div class="card" style="width: 10rem; height: 8rem; margin-left: 5px">
+            <img class="card-img-top" style="height: 45%; width:70%; align-self: center" src="../../../public/images/Coffee_cup_icon.svg.png">
+            <div class="card-body d-flex justify-content-between">
+                <p class="card-text">Coffee</p>
+                <p class="card-text">5$</p>
+            </div>
+        </div>
+
+        <div class="card" style="width: 10rem; height: 8rem; margin-left: 5px">
+            <img class="card-img-top" style="height: 45%; width:70%; align-self: center" src="../../../public/images/Coffee_cup_icon.svg.png">
+            <div class="card-body d-flex justify-content-between">
+                <p class="card-text">Coffee</p>
+                <p class="card-text">5$</p>
+            </div>
+        </div>
+
+        <div class="card" style="width: 10rem; height: 8rem; margin-left: 5px">
+            <img class="card-img-top" style="height: 45%; width:70%; align-self: center" src="../../../public/images/Coffee_cup_icon.svg.png">
+            <div class="card-body d-flex justify-content-between">
+                <p class="card-text">Coffee</p>
+                <p class="card-text">5$</p>
+            </div>
+        </div>
+
+        <div class="card" style="width: 10rem; height: 8rem; margin-left: 5px">
+            <img class="card-img-top" style="height: 45%; width:70%; align-self: center" src="../../../public/images/Coffee_cup_icon.svg.png">
+            <div class="card-body d-flex justify-content-between">
+                <p class="card-text">Coffee</p>
+                <p class="card-text">5$</p>
+            </div>
+        </div>
+
+        <div class="card" style="width: 10rem; height: 8rem; margin-left: 5px">
+            <img class="card-img-top" style="height: 45%; width:70%; align-self: center" src="../../../public/images/Coffee_cup_icon.svg.png">
+            <div class="card-body d-flex justify-content-between">
+                <p class="card-text">Coffee</p>
+                <p class="card-text">5$</p>
+            </div>
+        </div>
+
+        <div class="card" style="width: 10rem; height: 8rem; margin-left: 5px">
+            <img class="card-img-top" style="height: 45%; width:70%; align-self: center" src="../../../public/images/Coffee_cup_icon.svg.png">
+            <div class="card-body d-flex justify-content-between">
+                <p class="card-text">Coffee</p>
+                <p class="card-text">5$</p>
+            </div>
+        </div>
+
+        <div class="card" style="width: 10rem; height: 8rem; margin-left: 5px">
+            <img class="card-img-top" style="height: 45%; width:70%; align-self: center" src="../../../public/images/Coffee_cup_icon.svg.png">
+            <div class="card-body d-flex justify-content-between">
+                <p class="card-text">Coffee</p>
+                <p class="card-text">5$</p>
+            </div>
+        </div>
+
+        <div class="card" style="width: 10rem; height: 8rem; margin-left: 5px">
+            <img class="card-img-top" style="height: 45%; width:70%; align-self: center" src="../../../public/images/Coffee_cup_icon.svg.png">
+            <div class="card-body d-flex justify-content-between">
+                <p class="card-text">Coffee</p>
+                <p class="card-text">5$</p>
+            </div>
+        </div>
+
+        <div class="card" style="width: 10rem; height: 8rem; margin-left: 5px">
+            <img class="card-img-top" style="height: 45%; width:70%; align-self: center" src="../../../public/images/Coffee_cup_icon.svg.png">
+            <div class="card-body d-flex justify-content-between">
+                <p class="card-text">Coffee</p>
+                <p class="card-text">5$</p>
+            </div>
+        </div>
+    </div>
+
+
+
+</section>
+</body>
+
+</html>
+
+
+<?php
+
+?>
