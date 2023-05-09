841c7defb1096335f13114831294f0143a17387c
Hasnaa Mohamed <hasnaa@mauve.de>
2023-05-09 10:03:33 +0300

    adding style to products

--- views/admin/products/add_product.php
+++ views/admin/products/add_product.php
@@ -11,47 +11,81 @@ if (isset($_GET["old"])) {
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
     <title>Title</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css ">
+    <link rel="stylesheet" type="text/css" href="js/bootstrap.js ">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="style.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 
 <body>
-    <div class="container border border-3 w-25 ">
-        <form action="store.php" method="Post" enctype="multipart/form-data">
-            <div class="mb-3">
-                <label for="name" class="form-label">Name</label>
-                <input type="name" class="form-control" name="name" value="<?php if (isset($old_data['name'])) echo $old_data['name']; ?>">
-                <span class="text-danger"> <?php if (isset($errors['name'])) echo $errors['name']; ?> </span>
+    <form action="store.php" method="Post" enctype="multipart/form-data">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-7">
+                    <h1 class="text-left " id="hh">Add Product</h1>
+                </div>
+                <div class="col-md-5" id="c">
+                    <div class="raw">
+                        <div class="col-md-6">
+                            <h3 class="text-left" id="h">Add Product Form</h3>
+                        </div>
+                        <div class="col-md-6">
+                            <span class="glyphicon glyphicon-pencil " id="gg"></span>
+                        </div>
+                        <hr>
+                        <div class="row">
+                            <label class="label-col-md-2 control-label" id="l">Name</label>
+                            <div class="col-md-10">
+                                <input id="cc" type="name" class="form-control" name="name" value="<?php if (isset($old_data['name'])) echo $old_data['name']; ?>">
+                                <span class="text-danger"> <?php if (isset($errors['name'])) echo $errors['name']; ?> </span>
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="row">
+                            <label id="l" for="exampleInputprice1" class="label-col-md-2 control-label">price</label>
+                            <div class="col-md-10">
+                                <input id="cc" type="number" class="form-control" value="<?php if (isset($old_data['price'])) echo $old_data['price']; ?>" name='price' id="exampleInputprice1" aria-describedby="priceHelp">
+                                <span class="text-danger"> <?php if (isset($errors['price'])) echo $errors['price']; ?> </span>
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="col-md-10">
+                            <label id="l" class="label-col-md-2 control-label" for="admin">Available</label>
+                                <input type="checkbox" checked="true" value="1" name="available">
+                        </div>
+                        <hr>
+                        <div class="row">
+                            <label id="l" class="label-col-md-2 control-label" for="cat_id"> Category </label>
+                            <div class="col-md-10">
+                                <select class="form-control" id="cc" name="cat_id">
+                                    <option id="o" value="1">1</option>
+                                    <option id="o" value="2">2</option>
+                                    <option id="o" value="3">3</option>
+                                </select>
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="row">
+                            <label id="l" class="label-col-md-2 control-label">Image </label>
+                            <div class="col-md-10">
+                                <input type="file" name="image" class="form-control  w-100" id="image">
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+                    <button class="btn btn-primary" type="submit">Submit</button>
+                    <button class="btn btn-danger" type="reset">Cancle</button>
+
+                </div>
             </div>
-            <div class="mb-3">
-                <label for="exampleInputprice1" class="form-label">price</label>
-                <input type="number" class="form-control" value="<?php if (isset($old_data['price'])) echo $old_data['price']; ?>" name='price' id="exampleInputprice1" aria-describedby="priceHelp">
-                <div id="priceHelp" class="form-text">We'll never share your price with anyone else.</div>
-                <span class="text-danger"> <?php if (isset($errors['price'])) echo $errors['price']; ?> </span>
-            </div>
-            <div class="mb-3">
-                <label for="admin">Available</label>
-                <input style="margin-left: 10px;" type="checkbox" checked="true" value="1" name="available">
-            </div>
-            <div>
-                <label for="cat_id"> Category </label>
-                <select class="form-select" name="cat_id">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                </select>
-            </div>
-            <br>
-            <div class="mb-3">
-                <label class="form-label">Image </label>
-                <input type="file" name="image" class="form-control  w-100" id="image">
-            </div>
-            <button type="submit" class="btn btn-primary ">Add new Product</button>
-        </form>
-    </div>
-    </div>
+        </div>
+    </form>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 
